94644a57718010aba141ed158f5d479ecbe02f2e
Pedro Ortiz <pedrops.ortiz@gmail.com>
2021-02-14 23:01:49 -0300

    Inseri o valor do myName

--- chitchat.php
+++ chitchat.php
@@ -1,58 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
 
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/8.2.7/firebase-app.js"></script>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat</title>
 
-<script src="https://www.gstatic.com/firebasejs/8.2.7/firebase-database.js"></script>
+    <!-- The core Firebase JS SDK is always required and must be listed first -->
+    <script src="https://www.gstatic.com/firebasejs/8.2.7/firebase-app.js"></script>
 
-<!-- TODO: Add SDKs for Firebase products that you want to use
+    <script src="https://www.gstatic.com/firebasejs/8.2.7/firebase-database.js"></script>
+
+    <!-- TODO: Add SDKs for Firebase products that you want to use
      https://firebase.google.com/docs/web/setup#available-libraries -->
 
-<script>
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyC8ViCfYwgh1I9JIvAyafFmsZejXv1lLLg",
-    authDomain: "chit-chat-cf8b5.firebaseapp.com",
-    projectId: "chit-chat-cf8b5",
-    storageBucket: "chit-chat-cf8b5.appspot.com",
-    messagingSenderId: "298087203015",
-    appId: "1:298087203015:web:7e5e1503e3215a5c387ee2"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+    <script>
+        // Your web app's Firebase configuration
+        var firebaseConfig = {
+            apiKey: "AIzaSyC8ViCfYwgh1I9JIvAyafFmsZejXv1lLLg",
+            authDomain: "chit-chat-cf8b5.firebaseapp.com",
+            projectId: "chit-chat-cf8b5",
+            storageBucket: "chit-chat-cf8b5.appspot.com",
+            messagingSenderId: "298087203015",
+            appId: "1:298087203015:web:7e5e1503e3215a5c387ee2"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
 
-  //var myName = prompt("enter your name");
+        var myName = sessionStorage.getItem('nick');
 
-  function sendMessage() {
-        // get message
-        var message = document.getElementById("message").value;
- 
-        // save in database
-        firebase.database().ref("messages").push().set({
-            "sender": myName,
-            "message": message
+        function sendMessage() {
+            // get message
+            var message = document.getElementById("message").value;
+
+            // save in database
+            firebase.database().ref("messages").push().set({
+                "sender": myName,
+                "message": message
+            });
+
+            // prevent form from submitting
+            return false;
+        }
+
+        firebase.database().ref("messages").on("child_added", function(snapshot) {
+            var html = "";
+            html += "<li>";
+            html += snapshot.val().sender + ": " + snapshot.val().message;
+            html += "</li>";
+
+            document.getElementById("messages").innerHTML += html;
         });
- 
-        // prevent form from submitting
-        return false;
-    }
-
-    firebase.database().ref("messages").on("child_added", function (snapshot){
-        var html = "";
-        html += "<li>";
-        html += snapshot.val().sender + ": " + snapshot.val().message;
-        html += "</li>";
-
-        document.getElementById("messages").innerHTML += html;
-    });
-
-</script>
-
-<form onsubmit="return sendMessage()">
+    </script>
+</head>
+<body>
+    <form onsubmit="return sendMessage()">
         <input id="message" placeholder="enter a message" autocomplete="off">
         <input type="submit">
     </form>
-
-
-    <ul id="messages">
-
-    </ul>
+</body>
+</html>
+<ul id="messages">
+</ul>
